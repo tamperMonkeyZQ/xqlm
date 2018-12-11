73790a0450e65a2318b0f692231cdc406adc7d4c
YoungJin <919042140@qq.com>
2018-12-11 19:29:07 +0800

    cnm

--- src/main/java/com/no1/service/RecruitService.java
+++ src/main/java/com/no1/service/RecruitService.java
@@ -3,8 +3,9 @@ package com.no1.service;
 import com.no1.domain.Recruit;
 
 import java.util.List;
+import java.util.Map;
 
 public interface RecruitService {
-    List<Recruit> getAll(int pageNumber, int pageSize );
-    List<Recruit> search(int pageNumber, int pageSize , String searchVO);
+    Map<String,Object> getAll(int pageNumber, int pageSize );
+    Map<String,Object> search(int pageNumber, int pageSize , String searchVO);
 }
